ef2ac3fa9ec3299c93f20b1b08c471e58a42aa11
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-01-21 14:26:03 +0530

    commit

---
+++ reverse_number_using_loop.py
@@ -0,0 +1,9 @@
+#reverse a number using loop
+num=123
+st=str(num)
+rev=""
+for i in st:
+    rev=i+rev
+print("Reverse  of given number is ",rev)
+    
+
---
+++ reverse_using_loop.py
@@ -0,0 +1,9 @@
+#reverse a number using for loop
+num=123
+st=str(num)
+rev=""
+for i in st:
+    rev=i+rev
+print("Reverse  of given number is ",rev)
+    
+
