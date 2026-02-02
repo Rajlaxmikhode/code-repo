37790e4411eaef76ec8268b13175f8d2e91599f2
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-02-02 23:04:52 +0530

    excel automation using python

---
+++ project1.py
@@ -0,0 +1,28 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart,Reference
+
+wb = xl.load_workbook(r"C:\Users\Laptronics\Desktop\Git p\transactions.xlsx")
+sheet = wb.active
+
+for row in range(2,sheet.max_row+1):
+    cell=sheet.cell(row,3)
+    corrected_price=cell.value*0.9
+    corrected_price_cell=sheet.cell(row,4)
+    corrected_price_cell.value=corrected_price
+values=Reference(sheet,
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4)
+chart=BarChart()
+chart.add_data(values)
+chart.varyColors=True
+sheet.add_chart(chart,'e2')
+
+wb.save(r"C:\Users\Laptronics\Desktop\Git p\new_transactions.xlsx")
+
+
+
+
+
+
