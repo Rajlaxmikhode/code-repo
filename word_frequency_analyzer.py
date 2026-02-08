f66e7e175cf9b7a00de30c802d666145ca2a991d
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-02-08 19:08:23 +0530

    modified:word_frequency_analyzer

--- word_frequency_analyzer.py
+++ word_frequency_analyzer.py
@@ -1,5 +1,8 @@
 # word frequency analyzer
+import re
 text=input("Enter a string:").lower()
+
+text = re.sub(r'[^\w\s]', '', text)
 freq={}
 for word in text.split():
     if word in freq:
@@ -8,6 +11,7 @@ for word in text.split():
         freq[word]=1
 print("\nfrequencies is:")
 for word,count in freq.items():
-    print(f"{word}:{count}")     
-check_word=input("Enter a word to check it's frequency:  ").lower()
-print(f"{check_word} appeared {freq.get(check_word),0} times.")
+    print(f"{word}:{count}")   
+
+check_word=input("\nEnter a word to check it's frequency: ").lower()
+print(f"'{check_word}' appeared {freq.get(check_word),0} times.")
