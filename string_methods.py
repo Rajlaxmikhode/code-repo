4cb47e980890d4ebbccf24a1932a53ff77d3ffe0
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-02-10 22:16:38 +0530

    updated:string_method

--- string_methods.py
+++ string_methods.py
@@ -1,4 +1,5 @@
 st="Python_Programming"
+st2=" by guido van rossum"
 print(st.lower())
 print(st.upper())
 print(st.find("ython"))
@@ -15,3 +16,7 @@ print("py" not in st)
 print(s in st)
 print(s is not st)
 print(s.title())
+print(isinstance(st,str)) # returns true if the specified object is of specified type
+st+=st2 # performs both oncatenation and assignment opertion
+print(st)
+print(f'{st} is a high level programminng {st2}')# intterpolaration is the process of inserting variables and expressions directly into string 'f-string is one of the method'
