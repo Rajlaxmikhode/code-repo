f1efd8b5b0a23696244b1fef5ef220d694ea73b2
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-02-12 23:20:53 +0530

    Add example demonstrating str.maketrans and string translation

---
+++ Caesar_cipher_basic.py
@@ -0,0 +1,27 @@
+# shift means how many letters we move forward in the alphabet
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shift = 5
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+print(shifted_alphabet)
+
+'''
+The str.maketrans() method takes two strings of equal length and returns a translation table that maps each character 
+of the first string with the corresponding character of the second string. 
+Each character in the translation table is stored as a Unicode ordinal, 
+a number that uniquely identifies the character
+'''
+
+lower_chars="abc"
+upper_chars="ABC"
+table=str.maketrans(lower_chars,upper_chars)
+print(table)
+translation_table=str.maketrans(alphabet,shifted_alphabet)
+print(translation_table)
+"""
+maketrans() = making a dictionary of replacements
+translate() = applying those replacements to a string
+"""
+message = "ate"
+encrypted = message.translate(translation_table)
+print(encrypted)
