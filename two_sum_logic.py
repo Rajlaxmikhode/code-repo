005429a8a76564f2c34c03fe9bc2257f7b1307dd
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-02-13 18:06:34 +0530

    implemented two sum logic using nested loops

---
+++ two_sum_logic.py
@@ -0,0 +1,18 @@
+nums = [2, 7, 11, 15]
+target = 180
+found=False
+for i in range(len(nums)):
+    for  j in range(i+1,len(nums)):
+        if nums[i]+nums[j]==target:
+            print(f"[{i,j}]")
+            found=True
+            break
+    if found:
+        break
+if not found:
+    print("undefined")
+
+
+    
+    
+
