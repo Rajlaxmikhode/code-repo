4b5c51870dd6d00ea9465c33dffe0f124bd7f1a1
Rajlaxmikhode <khoderajlaxmi17@gmail.com>
2026-03-02 21:12:31 +0530

    add:number pattern generator

---
+++ number_pattern_generator.py
@@ -0,0 +1,13 @@
+def number_pattern(n):
+    if not isinstance(n,int):
+        return "Argument must be an integer value"
+    if n<1:
+        return "Argument must be an integer greater than 0"
+    res=''
+    for i in range(1, n+1):
+      res+=str(i)+' '
+    print(res)
+    return res.strip
+num=int(input("Enter any number: "))
+
+number_pattern(num)
